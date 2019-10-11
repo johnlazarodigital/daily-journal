4b9776686245245c92ea868dc565724b7ada9ac3
johnlazarodigital <johnlazarodigital@gmail.com>
2019-10-11 22:26:16 +0800

    Initial commit

---
+++ includes/class-daily-journal-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       johnlazaro.com
+ * @since      1.0.0
+ *
+ * @package    Daily_Journal
+ * @subpackage Daily_Journal/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Daily_Journal
+ * @subpackage Daily_Journal/includes
+ * @author     John Lazaro <johnlazarodigital@gmail.com>
+ */
+class Daily_Journal_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-daily-journal-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       johnlazaro.com
+ * @since      1.0.0
+ *
+ * @package    Daily_Journal
+ * @subpackage Daily_Journal/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Daily_Journal
+ * @subpackage Daily_Journal/includes
+ * @author     John Lazaro <johnlazarodigital@gmail.com>
+ */
+class Daily_Journal_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
